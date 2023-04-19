ba259011ddd62ee8fd20d81455813a369f4a7b56
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-19 17:11:48 -0400

    first attempt-not iterating

--- src/index.js
+++ src/index.js
@@ -1,4 +1,45 @@
 let addToy = false;
+let toyArry
+
+
+
+const toyList=document.querySelector("#toy-collection")
+
+function getToys () {
+  return fetch("http://localhost:3000/toys")
+    .then(resp=>resp.json())
+    .then (toys => {
+        toyArry=toys
+       // toysList.className="card"
+
+      toys.map(toys => {
+       // const toyCards=document.createElement()
+        createToyCard(toys)})}
+      )}
+
+const toyCard=document.createElement('li')
+const toyImg=document.createElement("img")
+const h2Tag= document.createElement("h2")
+const pLikes=document.createElement("p")
+const btn=document.createElement("button")
+
+function createToyCard(toys) {
+  // toyImg=document.querySelector("img")
+  // h2Tag=document.querySelector("h2")
+  // pLikes=document.querySelector("p")
+  // btn=document.querySelector("button")
+
+  toyCard.append(toyImg, h2Tag, pLikes, btn)
+
+  toyImg.src=toys.image
+  toyImg.alt=toys.name
+  h2Tag.textContent=toys.name
+  pLikes.textContent=toys.likes
+
+  toyList.append(toyCard)
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +54,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+getToys()
